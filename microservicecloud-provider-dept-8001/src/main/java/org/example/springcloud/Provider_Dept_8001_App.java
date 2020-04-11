916129fe5b38e1e1a0bdc61ac9f1fd94b2c22f72
zhangrq <zhangrongianglo@163.com>
2020-04-11 17:20:12 +0800

    update

--- microservicecloud-provider-dept-8001/src/main/java/org/example/springcloud/Provider_Dept_8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/org/example/springcloud/Provider_Dept_8001_App.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
-@EnableDiscoveryClient //服务发现
+//@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+//@EnableDiscoveryClient //服务发现
 public class Provider_Dept_8001_App
 {
 	public static void main(String[] args)
